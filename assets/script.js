888843f4bd4ad7c2beba8dd04c036d81a0f7b624
Acolyte03 <Acolyte03@gmail.com>
2023-09-27 16:03:13 -0500

    Added in my logic and properly concatenating arrays. Also fixed the issue where my arrays weren't concatenating correctly

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() 
+{
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var finishedPassword = document.querySelector("#password");
 
-  passwordText.value = password;
+  finishedPassword = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 var lowerCha = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", 
 "a", "s", "d", "f", "g", "h", "j", "k", "l", 
@@ -24,9 +24,51 @@ var nums = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCha = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", 
 ")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'",
 ",", "<", ".", ">", "/", "?"];
+var finishedPassword;
+var applicable = [];
+
+if(generateBtn.addEventListener("click", function generatePasswords()
+{
+  var numberOfChar = window.prompt("How many characters would you like your password to be?");
+  console.log(numberOfChar);
+
+  var yesSpecial = window.confirm("Would you like to include special characters in your password?");
+  console.log(yesSpecial);
+
+  var yesLower = window.confirm("Would you like to include lower case letters in your password?");
+  console.log(yesLower);
+
+  var yesUpper = window.confirm("Would you like to include Upper case letters in your password?");
+  console.log(yesUpper);
+
+  var yesNums = window.confirm("Would you like to include numbers in your password?");
+  console.log(yesNums);
+
+  if(yesSpecial)
+    applicable = applicable.concat(specialCha);
+
+  if(yesLower)
+    applicable = applicable.concat(lowerCha);
+
+  if(yesUpper)
+    applicable = applicable.concat(upperCha);
+
+  if(yesNums)
+    applicable = applicable.concat(nums);
+
+  for(i = 0; i < numberOfChar; i++)
+  {
+    finishedPassword = applicable[Math.floor(Math.random()*applicable.length)];
+    return finishedPassword;
+  }
+  console.log(finishedPassword);
+
+}))
+
 
 function generatePassword()
 {
+  
 // var numChar = window.prompt;
 // var includesomething = window.confirm; true/false
 
