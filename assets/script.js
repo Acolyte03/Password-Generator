e289bee93399266f371c02ba7a7578e3be2cda2e
Acolyte03 <Acolyte03@gmail.com>
2023-09-27 14:56:26 -0500

    Initialized array variables for each of my character types and expanded my pseudo-code

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,18 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+var lowerCha = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", 
+"a", "s", "d", "f", "g", "h", "j", "k", "l", 
+"z", "x", "c", "v", "b", "n", "m"];
+var upperCha = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", 
+"A", "S", "D", "F", "G", "H", "J", "K", "L", 
+"Z", "X", "C", "V", "B", "N", "M",];
+var nums = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCha = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", 
+")", "-", "_", "=", "+", "[", "{", "]", "}", "|", ";", ":", "'",
+",", "<", ".", ">", "/", "?"];
+
 function generatePassword()
 {
 // var numChar = window.prompt;
